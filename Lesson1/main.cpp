c0bb9c6729c9bacea53090636b745ab4469599da
OsipovStas <stasstels@gmail.com>
2013-09-30 15:56:41 +0400

    Lesson1 dir added

---
+++ Lesson1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: stasstels
+ *
+ * Created on September 30, 2013, 3:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
