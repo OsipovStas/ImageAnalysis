0e0c23cd7c647455b895cf09a705c108305febdc
OsipovStas <stasstels@gmail.com>
2013-10-19 19:22:47 +0400

    Lesson2 tasks 1-5

--- Lesson2/main.cpp
+++ Lesson2/main.cpp
@@ -15,14 +15,33 @@
 #include <cv.h>
 #include <highgui.h>
 
+
+// POSIX Specific
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
 typedef std::vector<cv::Mat>::iterator VMit;
 typedef cv::MatIterator_<uchar> Mit;
 
-static const std::string path("./build/Debug/gen/");
+static const std::string PATH("./build/Debug/gen/");
+
+static const double task1v[] = {0.5, 0, 1, -125, 2, 0, 2, -255, 1, 125};
+static const int task1c = 5;
+
+static const double task3v[] = {0.05, 0.1, 0.15f};
+static const int task3c = 3;
+
+static const double task4v[] = {0, 30, 0, 60, 50, 30};
+static const int task4c = 3;
+
+static const int task5v[] = {3, 3, 5, 5, 7, 7};
+static const int task5c = 3;
 
 void scaleImage(const cv::Mat& src, cv::Mat& dst, double alpha, double beta) {
     CV_Assert(src.channels() == 3);
-    
+
     dst = alpha * src + cv::Scalar(beta, beta, beta);
 }
 
@@ -30,7 +49,7 @@ void concatChannels(const cv::Mat& src, cv::Mat& dst) {
     std::vector<cv::Mat> channels;
     cv::split(src, channels);
     dst = cv::Mat();
-    for(VMit vmit = channels.begin(); vmit != channels.end(); ++vmit) {
+    for (VMit vmit = channels.begin(); vmit != channels.end(); ++vmit) {
         dst.push_back(*vmit);
     }
 }
@@ -40,45 +59,45 @@ void concatImages(const cv::Mat& image1, const cv::Mat& image2, cv::Mat& dst) {
     CV_Assert(image1.type() == image2.type());
     CV_Assert(image1.channels() == image2.channels());
 
-    dst = cv::Mat(image1.rows, image1.cols + image2.cols, image1.type());
-    cv::Mat tmp1 = dst.colRange(0, image1.cols);
-    cv::Mat tmp2 = dst.colRange(image1.cols, dst.cols);
+    cv::Mat tmp(image1.rows, image1.cols + image2.cols, image1.type());
+    cv::Mat tmp1 = tmp.colRange(0, image1.cols);
+    cv::Mat tmp2 = tmp.colRange(image1.cols, tmp.cols);
     image1.copyTo(tmp1);
     image2.copyTo(tmp2);
+    dst = tmp;
 }
 
 bool task1(const cv::Mat& image) {
-    cv::Mat manual, buildIn, diff;
+    cv::Mat manual, buildIn, diff, tmp;
     std::vector<cv::Mat> channels;
-    
+
     scaleImage(image, manual, 2, 100);
     image.convertTo(buildIn, -1, 2, 100);
     cv::absdiff(manual, buildIn, diff);
     cv::split(diff, channels);
-    
+
     std::cout << "Max difference element: ";
-    for(VMit it = channels.begin(); it != channels.end(); ++it) {
+    for (VMit it = channels.begin(); it != channels.end(); ++it) {
         int max = *(std::max_element((*it).begin<uchar>(), (*it).end<uchar>()));
         std::cout << max << " ";
     }
     std::cout << std::endl;
-    
-    cv::Mat scale1, scale2, scale3, scale4, scale5, dst1, dst2, tmp;
-    scaleImage(image, scale1, 0.5, 0);
-    scaleImage(image, scale2, 1, -125);
-    scaleImage(image, scale3, 2, 0);
-    scaleImage(image, scale4, 2, -255);
-    scaleImage(image, scale5, 1, 125);
 
-    concatImages(scale1, scale2, dst1);
-    concatChannels(dst1, dst1);
-   
-    concatImages(scale3, scale4, tmp);
-    concatImages(tmp, scale5, dst2);
-    concatChannels(dst2, dst2);
+    std::vector<cv::Mat> scales(5, cv::Mat());
+    std::vector<cv::Mat> dst(2, cv::Mat());
+    for (int i = 0; i < task1c; ++i) {
+        scaleImage(image, scales[i], task1v[2 * i], task1v[2 * i + 1]);
+    }
 
-    return cv::imwrite(path + "Task1Lena01.jpg", dst1) && cv::imwrite(path + "Task1Lena345.jpg", dst2);
-    
+    concatImages(scales[0], scales[1], dst[0]);
+    concatChannels(dst[0], dst[0]);
+
+    concatImages(scales[2], scales[3], dst[1]);
+    concatImages(dst[1], scales[4], dst[1]);
+    concatChannels(dst[1], dst[1]);
+
+    return cv::imwrite(PATH + "Task1Lena01.jpg", dst[0]) && cv::imwrite(PATH + "Task1Lena345.jpg", dst[1]);
+
 }
 
 bool task2(const cv::Mat& image) {
@@ -91,73 +110,148 @@ bool task2(const cv::Mat& image) {
     res.push_back(normalized);
     res.push_back(equalized);
     res.push_back(diff);
-    
-    return cv::imwrite(path + "Task2.jpg", res);
+
+    return cv::imwrite(PATH + "Task2.jpg", res);
 }
 
 void addSaltAndPepper(const cv::Mat& src, cv::Mat& dst, double p, double q) {
     CV_Assert(src.type() == CV_8UC1);
-    
+
     cv::RNG rng;
     src.copyTo(dst);
-    for(Mit mit = dst.begin<uchar>(); mit != dst.end<uchar>(); ++mit) {
+    for (Mit mit = dst.begin<uchar>(); mit != dst.end<uchar>(); ++mit) {
         double r = rng.uniform(0.0, 1.0);
-        if(r < p) {
+        if (r < p) {
             *mit = 255;
             continue;
         }
-        if(r < p + q) {
+        if (r < p + q) {
             *mit = 0;
         }
     }
 }
 
-std::string generateName(const std::string& name, int suff = 0, const char* ext = ".jpg") {
-    std::string res(name);
-    if(suff) {
-        res += ((char)(((int)'0') + suff));
+std::string generateName(const std::string& base, int suff = 0, const char* ext = ".jpg") {
+    std::string res(base);
+    if (suff) {
+        res += ((char) (((int) '0') + suff));
     }
     res += ext;
     return res;
 }
 
 bool task3(const cv::Mat& image) {
-    std::vector<cv::Mat> channels;
-    cv::split(image, channels);
     int i = 1;
     cv::Mat tmp;
     bool res = true;
 
-    for(VMit vmit = channels.begin(); vmit != channels.end(); ++vmit) {
-        addSaltAndPepper(*vmit, tmp, 0.05, 0.05);
-        res &= cv::imwrite(path + generateName("Task3", i++), tmp);
-        addSaltAndPepper(*vmit, tmp, 0.1, 0.1);
-        res &= cv::imwrite(path + generateName("Task3", i++), tmp);
-        addSaltAndPepper(*vmit, tmp, 0.15, 0.15);
-        res &= cv::imwrite(path + generateName("Task3", i++), tmp);
+    for (const double* dp = task3v; dp != task3v + task3c; ++dp) {
+        std::vector<cv::Mat> channels;
+        cv::split(image, channels);
+        for (VMit vmit = channels.begin(); vmit != channels.end(); ++vmit) {
+            addSaltAndPepper(*vmit, *vmit, *dp, *dp);
+        }
+        cv::merge(channels, tmp);
+        res &= cv::imwrite(PATH + generateName("noize/Task3-", i++), tmp);
     }
     return res;
 }
 
+void addAdditiveNoize(const cv::Mat& src, cv::Mat& dst, double mean, double stddev) {
+    CV_Assert(src.type() == CV_8UC1);
+
+    cv::RNG rng;
+    src.copyTo(dst);
+    cv::Mat noize(dst.rows, dst.cols, dst.type(), cv::Scalar(0));
+    cv::randn(noize, mean, stddev);
+    dst += noize;
+}
+
+bool task4(const cv::Mat& image) {
+    int suff = 1;
+    cv::Mat tmp;
+    bool res = true;
+
+    for (int i = 0; i < task4c; ++i) {
+        std::vector<cv::Mat> channels;
+        cv::split(image, channels);
+        for (VMit vmit = channels.begin(); vmit != channels.end(); ++vmit) {
+            addAdditiveNoize(*vmit, *vmit, task4v[2 * i], task4v[2 * i + 1]);
+        }
+        cv::merge(channels, tmp);
+        res &= cv::imwrite(PATH + generateName("noize/Task4-", suff++), tmp);
+    }
+    return res;
+}
+
+void filterImage(const cv::Mat& image, cv::Mat& dst) {
+    dst = cv::Mat(image.rows * 3, 0, image.type());
+    std::vector<cv::Mat> filters(3, cv::Mat());
+    std::vector<cv::Mat> result(task5c, cv::Mat());
+    for (int i = 0; i < task5c; ++i) {
+        cv::blur(image, filters[0], cv::Size(task5v[2 * i], task5v[2 * i + 1]));
+        cv::GaussianBlur(image, filters[1], cv::Size(task5v[2 * i], task5v[2 * i + 1]), 0);
+        cv::medianBlur(image, filters[2], task5v[2 * i]);
+        for(VMit vmit = filters.begin(); vmit != filters.end(); ++vmit) {
+            result[i].push_back(*vmit);
+        }
+    }
+    for(VMit vmit = result.begin(); vmit != result.end(); ++vmit) {
+        concatImages(dst, *vmit, dst);
+    }
+}
+
+bool task5(const char* path) {
+    struct dirent *entry;
+    DIR *dp;
+    bool res = true;
+    int i = 1;
+    
+    dp = opendir(path);
+    if (dp == NULL) {
+        std::cerr << "opendir: Path does not exist or could not be read." << std::endl;
+        return -1;
+    }
+
+    while ((entry = readdir(dp))) {
+        struct stat s;
+        std::string filename(path);
+        filename += (entry -> d_name);
+        if (stat(filename.c_str(), &s) == 0) {
+            if (s.st_mode & S_IFREG) {
+                cv::Mat image, filtered;
+                image = cv::imread(filename.c_str(), 1);
+                filterImage(image, filtered);
+                res &= cv::imwrite(PATH + generateName("Task5-", i++), filtered);
+            }
+        }
+    }
+
+    closedir(dp);
+    return true;
+}
 
 int main(int argc, char** argv) {
-    
-    if(argc != 2) {
+
+    if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " path/to/lena" << std::endl;
         exit(1);
     }
-    
+
     cv::Mat image;
     image = cv::imread(argv[1], 1);
-    
+
     std::cout << "Task 1 Status: " << (task1(image) ? "OK" : "Error") << std::endl;
     std::cout << "Task 2 Status: " << (task2(image) ? "OK" : "Error") << std::endl;
     std::cout << "Task 3 Status: " << (task3(image) ? "OK" : "Error") << std::endl;
+    std::cout << "Task 4 Status: " << (task4(image) ? "OK" : "Error") << std::endl;
+    std::cout << "Task 5 Status: " << (task5((PATH + "noize/").c_str()) ? "OK" : "Error") << std::endl;
 
-    
-//    cv::namedWindow( "Lesson 2", CV_WINDOW_NORMAL);
-//    cv::imshow( "Lesson 2", dst);                   
-//    cv::waitKey(0);    
+
+
+    //    cv::namedWindow( "Lesson 2", CV_WINDOW_NORMAL);
+    //    cv::imshow( "Lesson 2", dst);                   
+    //    cv::waitKey(0);    
     return 0;
 
 }
