e9f00c43ce193bf9c9f83c3f720cc99bb710dc0c
Osipov Stanislav <stasstels@gmail.com>
2013-10-23 21:59:50 +0400

    Task 1.1

--- Lesson3/main.cpp
+++ Lesson3/main.cpp
@@ -5,12 +5,10 @@
  * Created on October 22, 2013, 6:40 PM
  */
 
-
 #include <cmath>
 
 #include <iostream>
 #include <vector>
-
 
 #include <cv.h>
 #include <highgui.h>
@@ -20,67 +18,80 @@ static const std::string res("./res/");
 
 using namespace cv;
 
-
-
-cv::Mat src;
-cv::Mat src_gray;
 int thresh = 100;
 int max_thresh = 255;
 cv::RNG rng(12345);
 
-/**
- * @function thresh_callback
- */
-void thresh_callback(int, void*) {
+bool cmpX(const cv::Point& p1, const cv::Point& p2) {
+    return p1.x < p2.x;
+}
+
+bool cmpY(const cv::Point& p1, const cv::Point& p2) {
+    return p1.y < p2.y;
+}
+
+bool cmpContourLen(const std::vector<cv::Point>& c1, const std::vector<cv::Point>& c2) {
+    return c1.size() > c2.size();
+}
+
+std::string genNumberString(int number) {
+    char buf[16];
+    sprintf(buf, "%d", number);
+    return std::string(buf);
+}
+
+cv::Point getCircleCenter(const std::vector<cv::Point>& circleContour) {
+    int maxX = (*std::max_element(circleContour.begin(), circleContour.end(), cmpX)).x;
+    int maxY = (*std::max_element(circleContour.begin(), circleContour.end(), cmpY)).y;
+    int minX = (*std::min_element(circleContour.begin(), circleContour.end(), cmpX)).x;
+    int minY = (*std::min_element(circleContour.begin(), circleContour.end(), cmpY)).y;
+
+    int xc = (maxX + minX) / 2;
+    int yc = (maxY + minY) / 2;
+
+    return cv::Point(xc, yc);
+}
+
+bool task1_1() {
+    cv::Mat image, gray;
+
+    image = cv::imread(res + "coins_1.jpg", 1);
+    cv::cvtColor(image, gray, CV_BGR2GRAY);
+    cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
+
     cv::Mat canny_output;
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
 
     /// Detect edges using canny
-    cv::Canny(src_gray, canny_output, thresh, thresh * 2, 3);
+    cv::Canny(gray, canny_output, thresh, thresh * 2, 3);
     /// Find contours
-    cv::findContours(canny_output, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+    cv::findContours(canny_output, contours, hierarchy, CV_RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
 
+    std::sort(contours.begin(), contours.end(), cmpContourLen);
     /// Draw contours
     cv::Mat drawing = cv::Mat::zeros(canny_output.size(), CV_8UC3);
     for (size_t i = 0; i < contours.size(); i++) {
-        Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
-        drawContours(drawing, contours, (int) i, color, 2, 8, hierarchy, 0, Point());
-    }
-
-    
-    
-    /// Show in a window
-    namedWindow("Contours", WINDOW_NORMAL);
-    imshow("Contours", drawing);
-}
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    src = cv::imread(res + "coins_5_3.jpg", 1);
-
-    cv::cvtColor(src, src_gray, CV_BGR2GRAY);
-    cv::medianBlur(src_gray, src_gray, 5);
-    cv::Mat lut(1, 256, CV_8U);
-    
-    for(int i = 0; i < 256; ++i) {
-        lut.at<uchar>(0, i) = i;
+        Scalar color = Scalar(rng.uniform(100, 255), rng.uniform(100, 255), rng.uniform(100, 255));
+        drawContours(drawing, contours, (int) i, color, -1, 8, hierarchy, 0, Point());
+        cv::putText(drawing, genNumberString(i + 1), getCircleCenter(contours[i]) + cv::Point(-10, 10), FONT_HERSHEY_SIMPLEX,
+                0.5, cv::Scalar(0, 0, 0));
     }
     
-    lut.colRange(120, 256) = cv::Scalar(0);
-    
-    cv::LUT(src_gray, lut, src_gray);
-    
-    cv::namedWindow("Lesson3", CV_WINDOW_NORMAL);
-    cv::imshow("Lesson3", src_gray);
-    cv::waitKey(0);
-
-    cv::createTrackbar(" Canny thresh:", "Source", &thresh, max_thresh, thresh_callback);
-    thresh_callback(0, 0);
-    cv::waitKey(0);
-
-    return 0;
+    return cv::imwrite(path + "Task1_1.jpg", drawing);
 }
+
+int main() {
+    std::cout << "Task 1.1 Status: " << (task1_1() ? "OK" : "ERROR") << std::endl;
+}
+
+//    cv::Mat lut(1, 256, CV_8U);
+
+
+//for (int i = 0; i < 256; ++i) {
+//    lut.at<uchar>(0, i) = i;
+//}
+//
+//lut.colRange(120, 256) = cv::Scalar(0);
+//
+//cv::LUT(src_gray, lut, src_gray);
