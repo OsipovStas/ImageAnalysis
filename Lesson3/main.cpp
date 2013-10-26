37e79849504559b8240be3bd908ec310dee1714f
Osipov Stanislav <stasstels@gmail.com>
2013-10-26 23:35:03 +0400

    Task 1.2

--- Lesson3/main.cpp
+++ Lesson3/main.cpp
@@ -34,6 +34,19 @@ bool cmpContourLen(const std::vector<cv::Point>& c1, const std::vector<cv::Point
     return c1.size() > c2.size();
 }
 
+bool cmpCircleRadius(const std::vector<cv::Point>& c1, const std::vector<cv::Point>& c2) {
+    cv::Point2f center1;
+    float r1;
+    cv::minEnclosingCircle(c1, center1, r1);
+
+    cv::Point2f center2;
+    float r2;
+    cv::minEnclosingCircle(c2, center2, r2);
+
+    return r1 > r2;
+}
+
+
 std::string genNumberString(int number) {
     char buf[16];
     sprintf(buf, "%d", number);
@@ -41,15 +54,11 @@ std::string genNumberString(int number) {
 }
 
 cv::Point getCircleCenter(const std::vector<cv::Point>& circleContour) {
-    int maxX = (*std::max_element(circleContour.begin(), circleContour.end(), cmpX)).x;
-    int maxY = (*std::max_element(circleContour.begin(), circleContour.end(), cmpY)).y;
-    int minX = (*std::min_element(circleContour.begin(), circleContour.end(), cmpX)).x;
-    int minY = (*std::min_element(circleContour.begin(), circleContour.end(), cmpY)).y;
+    cv::Point2f center;
+    float radius;
+    cv::minEnclosingCircle(circleContour, center, radius);
 
-    int xc = (maxX + minX) / 2;
-    int yc = (maxY + minY) / 2;
-
-    return cv::Point(xc, yc);
+    return cv::Point((int) (center.x), (int) (center.y));
 }
 
 bool task1_1() {
@@ -77,12 +86,45 @@ bool task1_1() {
         cv::putText(drawing, genNumberString(i + 1), getCircleCenter(contours[i]) + cv::Point(-10, 10), FONT_HERSHEY_SIMPLEX,
                 0.5, cv::Scalar(0, 0, 0));
     }
-    
+
     return cv::imwrite(path + "Task1_1.jpg", drawing);
+}
+
+bool task1_2() {
+    cv::Mat image, gray, coins, text;
+
+    image = cv::imread(res + "coins_2.jpg", 1);
+    cv::cvtColor(image, gray, CV_BGR2GRAY);
+    cv::GaussianBlur(gray, gray, cv::Size(3, 3), 0);
+    cv::threshold(gray, gray, 240, 255, CV_8U);
+    cv::dilate(gray, gray, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)), cv::Point(-1, -1), 3);
+    cv::erode(gray, gray, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(11, 11)), cv::Point(-1, -1), 2);
+    cv::cvtColor(gray, gray, CV_GRAY2BGR);
+
+    coins = image - gray;
+    gray = cv::Scalar(255, 255, 255) - gray;
+
+    text = image + cv::Scalar(1, 1, 1);
+    text -= gray;
+
+    cv::Mat lut(1, 256, CV_8U, cv::Scalar(255));
+
+    for (int i = 1; i < 256; ++i) {
+        lut.at<uchar>(0, i) = i;
+    }
+
+    cv::LUT(text, lut, text);
+
+    //fills black area by white
+    cv::floodFill(coins, cv::Point(1, 1), cv::Scalar(255, 255, 255));
+    return cv::imwrite(path + "Task1_2_coins.jpg", coins) &&
+            cv::imwrite(path + "Task1_2_text.jpg", text);
 }
 
 int main() {
     std::cout << "Task 1.1 Status: " << (task1_1() ? "OK" : "ERROR") << std::endl;
+    std::cout << "Task 1.2 Status: " << (task1_2() ? "OK" : "ERROR") << std::endl;
+
 }
 
 //    cv::Mat lut(1, 256, CV_8U);
